092e975e6abc6fa8f859e58b9a5c09ae0e340ef4
Raulek123 <wywial.krzysztof@wp.pl>
2023-09-17 13:47:50 +0200

    Rozbudowa aplikacji.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { StudentsListComponent } from './students-list/students-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateStudentComponent } from './create-student/create-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    StudentsListComponent,
+    CreateStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/students-list/students-list.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { StudentService } from '../student.service';
+import { Student } from '../student';
+import { delay } from 'rxjs';
+
+
+@Component({
+  selector: 'app-students-list',
+  templateUrl: './students-list.component.html',
+  styleUrls: ['./students-list.component.css']
+})
+export class StudentsListComponent {
+  isLoaded = false;
+  btnTitle = "Pokaż";
+  students: Student[] = [];
+  isDataSearching = false;
+  isErrorOccured = false;
+
+  constructor(private studentService: StudentService) {
+
+  }
+
+  search() {
+    console.log("Kliknięto przycisk Wyszukaj!");
+    this.isLoaded = !this.isLoaded;
+
+    if (this.isLoaded) {
+
+      this.isDataSearching = true;
+      this.btnTitle = "Wyszukiwanie...";
+      this.students = [];
+
+      this.studentService.getStudents()
+        .pipe(delay(2000))
+        .subscribe({
+          next: data => {
+            console.log("wewnatrz subscribe");
+            console.log(data);
+            this.students = data;
+
+            this.isDataSearching = false;
+            this.btnTitle = "Ukryj"
+          },
+          error: ()=>{
+            this.isDataSearching = false;
+            this.isLoaded = false;
+            this.btnTitle = "Pokaż"
+            this.isErrorOccured = true;
+          },
+        });
+
+      console.log("poza subscribe");
+    } else {
+      this.btnTitle = "Pokaż";
+    }
+
+  }
+}
